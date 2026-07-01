d390ce792f027cd6c95809731cfdfc063e6f864c
Shivang Tiwari <shivangt002@gmail.com>
2026-07-01 16:22:49 +0530

    Update db connection for Railway

--- auth/config/db.php
+++ auth/config/db.php
@@ -7,14 +7,13 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->safeLoad();
 
 $conn = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "mindcare_ai"
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME'],
+    (int) $_ENV['DB_PORT']
 );
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
-
-?>
